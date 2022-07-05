1030f9d7495413cda8863c4149a5b2e2998e9e66
zivnadel <zywn1414@gmail.com>
2022-07-05 21:29:44 +0300

    Added new project - EdgeLedger website

---
+++ EdgeLedger-website/js/blog.js
@@ -0,0 +1,18 @@
+const navbar = document.getElementById("navbar");
+let scrolled = false;
+
+window.onscroll = () => {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove("top");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(() => {
+      navbar.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add("top");
+    scrolled = false;
+  }
+};
